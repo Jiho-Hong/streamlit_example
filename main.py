413f790b0c27ea8aa2020a54c59a56bb9a98590e
Jiho-Hong <hongzoh@gmail.com>
2023-04-27 12:18:55 +0900

    Disable inference module

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 
-from sts_inference import sts_inference
+# from sts_inference import sts_inference
 
 st.title("Sentence Similarity")
 
@@ -14,12 +14,12 @@ if st.button("Calculate"):
     elif sentence2 == "":
         st.warning("Input sentence2")
     else:
-        placeholder = st.empty()
+        #placeholder = st.empty()
 
         similarity = None
 
-        with st.spinner("Wait for calculating how close"):
-            similarity = sts_inference(sentence1, sentence2)
+        #with st.spinner("Wait for calculating how close"):
+        #    similarity = sts_inference(sentence1, sentence2)
 
-        with st.container():
-            placeholder.success(f"Two sentences are {similarity}% similar.")
+        #with st.container():
+        #    placeholder.success(f"Two sentences are {similarity}% similar.")
